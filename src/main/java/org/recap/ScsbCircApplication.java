fa3461ccaba57849c101c80449dd38a3b55bc0ab
periinc <dev@periinc.com>
2016-11-10 08:29:26 -0500

    First commit

---
+++ src/main/java/org/recap/ScsbCircApplication.java
@@ -0,0 +1,12 @@
+package org.recap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScsbCircApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScsbCircApplication.class, args);
+	}
+}
