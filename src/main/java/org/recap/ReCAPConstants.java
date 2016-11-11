53a9a818f86a879652fc79698e11596c819b6f11
sudhish74 <sudhish.kutty@htcindia.com>
2016-11-11 13:33:11 +0530

    New File

---
+++ src/main/java/org/recap/ReCAPConstants.java
@@ -0,0 +1,16 @@
+
+package org.recap;
+
+/**
+ * Created by premkb on 19/8/16.
+ */
+public class ReCAPConstants {
+
+    public static final String FAILURE = "Failure";
+    public static final String SUCCESS = "Success";
+
+    public static final String COLUMBIA = "CUL";
+    public static final String PRINCETON = "PUL";
+    public static final String NYPL = "NYPL";
+
+}
