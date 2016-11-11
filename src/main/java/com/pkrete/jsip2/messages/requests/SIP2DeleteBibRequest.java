f16528858cb8bd3a541a756b62ce0ca536007640
sudhish74 <sudhish.kutty@htcindia.com>
2016-11-11 13:28:42 +0530

    Add new

---
+++ src/main/java/com/pkrete/jsip2/messages/requests/SIP2DeleteBibRequest.java
@@ -0,0 +1,72 @@
+package com.pkrete.jsip2.messages.requests;
+
+import com.pkrete.jsip2.util.MessageUtil;
+import com.pkrete.jsip2.util.StringUtil;
+
+/**
+ * Created by sudhishk on 9/11/16.
+ */
+public class SIP2DeleteBibRequest extends SIP2CirculationTransactionRequest {
+
+    private SIP2DeleteBibRequest(){
+        super("85");
+        this.itemIdentifier = "";
+        this.patronIdentifier ="";
+        this.currentLocation = "";
+        this.institutionId = "";
+        this.titleIdentifier ="";
+        this.terminalPassword = "";
+    }
+
+    public SIP2DeleteBibRequest(String staffIdentifier, String titleIdentififier, String itemIdIdentifier) {
+        this();
+        this.itemIdentifier = itemIdIdentifier;
+        this.titleIdentifier =titleIdentififier;
+        this.patronIdentifier=staffIdentifier;
+    }
+
+    public SIP2DeleteBibRequest(String staffIdentifier, String titleIdentififier, String itemIdIdentifier,String bibId) {
+        this();
+        this.itemIdentifier = itemIdIdentifier;
+        this.titleIdentifier =titleIdentififier;
+        this.patronIdentifier=staffIdentifier;
+        this.bibId=bibId;
+    }
+
+    @Override
+    public String getData() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(this.code);
+        builder.append(StringUtil.bool2Char(this.noBlock));
+        builder.append(this.transactionDate);
+        builder.append("|AO");
+        builder.append(this.institutionId);
+        builder.append("|AA");
+        builder.append(this.patronIdentifier);
+        builder.append("AP");
+        builder.append(this.currentLocation);
+        builder.append("|AB");
+        builder.append(this.itemIdentifier);
+        builder.append("|AC");
+        builder.append(this.terminalPassword);
+        if (titleIdentifier != null) {
+            builder.append("|AJ");
+            builder.append(titleIdentifier);
+        }
+        if(this.bibId != null) {
+            builder.append("|MA");
+            builder.append(this.bibId);
+        }
+
+        builder.append("|");
+        if(errorDetectionEnabled) {
+            builder.append("AY");
+            builder.append(this.getSequence());
+            builder.append("AZ");
+            this.checkSum = MessageUtil.computeChecksum(builder.toString());
+            builder.append(this.checkSum);
+        }
+
+        return builder.toString() + '\r';
+    }
+}
