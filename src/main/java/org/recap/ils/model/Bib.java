dc611946395a2d4498ac54936040705f93aee276
sudhish74 <sudhish.kutty@htcindia.com>
2016-12-26 16:08:18 +0530

    NYPL

---
+++ src/main/java/org/recap/ils/model/Bib.java
@@ -0,0 +1,45 @@
+package org.recap.ils.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+/**
+ * Created by pvsubrah on 6/21/16.
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Bib implements Serializable {
+    @XmlElement
+    private String owningInstitutionId;
+    @XmlElement
+    private String owningInstitutionBibId;
+    @XmlElement(required = true, nillable = true)
+    protected ContentType content;
+
+    public String getOwningInstitutionBibId() {
+        return owningInstitutionBibId;
+    }
+
+    public void setOwningInstitutionBibId(String owningInstitutionBibId) {
+        this.owningInstitutionBibId = owningInstitutionBibId;
+    }
+
+    public String getOwningInstitutionId() {
+        return owningInstitutionId;
+    }
+
+    public void setOwningInstitutionId(String owningInstitutionId) {
+        this.owningInstitutionId = owningInstitutionId;
+    }
+
+    public ContentType getContent() {
+        return content;
+    }
+
+    public void setContent(ContentType content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/org/recap/ils/model/Items.java
@@ -0,0 +1,26 @@
+package org.recap.ils.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+/**
+ * Created by pvsubrah on 6/21/16.
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Items implements Serializable {
+
+    @XmlElement(required = true, nillable = true)
+    protected ContentType content;
+
+    public ContentType getContent() {
+        return content;
+    }
+
+    public void setContent(ContentType content) {
+        this.content = content;
+    }
+}
