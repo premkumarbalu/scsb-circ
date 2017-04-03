c42f6b8694e97c6cece5985c3b03879ea029cb60
premkumarbalu <prem143india>
2017-04-03 17:19:33 +0530

    RECAP-587 - Added code changes for submit collection file process

--- src/main/java/org/recap/camel/submitcollection/SubmitCollectionPollingFtpRouteBuilder.java
+++ src/main/java/org/recap/camel/submitcollection/SubmitCollectionPollingFtpRouteBuilder.java
@@ -33,11 +33,6 @@ public class SubmitCollectionPollingFtpRouteBuilder {
     @Value("${ftp.privateKey}")
     private String ftpPrivateKey;
 
-/*    @Value("${ftp.submitcollection.remote.server}")
-    private String ftpRemoteServer;*/
-
-    //private ApplicationContext applicationContext;
-
     public SubmitCollectionPollingFtpRouteBuilder(CamelContext camelContext,ApplicationContext applicationContext,
                                                   @Value("${ftp.userName}") String ftpUserName, @Value("${ftp.submitcollection.pul}") String pulFtpFolder,
                                                   @Value("${ftp.submitcollection.cul}") String culFtpFolder, @Value("${ftp.submitcollection.nypl}") String nyplFtpFolder,
@@ -49,8 +44,6 @@ public class SubmitCollectionPollingFtpRouteBuilder {
                 public void configure() throws Exception {
                     from(ReCAPConstants.SFTP + ftpUserName + ReCAPConstants.AT + pulFtpFolder + ReCAPConstants.PRIVATE_KEY_FILE + ftpPrivateKey + ReCAPConstants.KNOWN_HOST_FILE + ftpKnownHost+ReCAPConstants.SUBMIT_COLLECTION_SFTP_OPTIONS+pulWorkDir)
                             .bean(applicationContext.getBean(SubmitCollectionProcessor.class,ReCAPConstants.PRINCETON),ReCAPConstants.PROCESS_INPUT)
-                            //.bean(new SubmitCollectionProcessor("PUL"),ReCAPConstants.PROCESS_INPUT)
-                            //.bean(SubmitCollectionProcessor.class,ReCAPConstants.PROCESS_INPUT)
                     ;
                 }
             });
@@ -60,8 +53,6 @@ public class SubmitCollectionPollingFtpRouteBuilder {
                 public void configure() throws Exception {
                     from(ReCAPConstants.SFTP + ftpUserName + ReCAPConstants.AT + culFtpFolder + ReCAPConstants.PRIVATE_KEY_FILE + ftpPrivateKey + ReCAPConstants.KNOWN_HOST_FILE + ftpKnownHost+ReCAPConstants.SUBMIT_COLLECTION_SFTP_OPTIONS+culWorkDir)
                             .bean(applicationContext.getBean(SubmitCollectionProcessor.class,ReCAPConstants.COLUMBIA),ReCAPConstants.PROCESS_INPUT)
-                            //.bean(new SubmitCollectionProcessor("CUL"),ReCAPConstants.PROCESS_INPUT)
-                            //.bean(SubmitCollectionProcessor.class,ReCAPConstants.PROCESS_INPUT)
 
                     ;
                 }
@@ -71,9 +62,7 @@ public class SubmitCollectionPollingFtpRouteBuilder {
                 @Override
                 public void configure() throws Exception {
                     from(ReCAPConstants.SFTP + ftpUserName + ReCAPConstants.AT + nyplFtpFolder + ReCAPConstants.PRIVATE_KEY_FILE + ftpPrivateKey + ReCAPConstants.KNOWN_HOST_FILE + ftpKnownHost+ReCAPConstants.SUBMIT_COLLECTION_SFTP_OPTIONS+nyplWorkDir)
-                            //.bean(SubmitCollectionProcessor.class,ReCAPConstants.PROCESS_INPUT)
                             .bean(applicationContext.getBean(SubmitCollectionProcessor.class,ReCAPConstants.NYPL),ReCAPConstants.PROCESS_INPUT)
-                            //.bean(new SubmitCollectionProcessor("NYPL"),ReCAPConstants.PROCESS_INPUT)
 
                     ;
                 }
