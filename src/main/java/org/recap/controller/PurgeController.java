527ffee400c2309d7fc52cc90b50c04745df0d9d
sudhish74 <sudhish.kutty@htcindia.com>
2017-06-14 12:57:31 +0530

    Java Doc

--- src/main/java/org/recap/controller/PurgeController.java
+++ src/main/java/org/recap/controller/PurgeController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
 
 /**
+ * This controller is intended to be used from scsb-schduler project, The methods are used for deleting information from certain columns or records from certain tables, after certain period.
+ *
  * Created by hemalathas on 10/4/17.
  */
 @RestController
@@ -20,18 +22,35 @@ public class PurgeController {
     @Autowired
     private PurgeService purgeService;
 
+    /**
+     * Purge email address  from request_t table after certain period.
+     * This is required, part of security feature, as patron information(Email), should be stored only of certain period in SCSB.
+     *This method will be accesssed from a schduled job.
+     *
+     * @return the response entity
+     */
     @RequestMapping(value = "/purgeEmailAddress", method = RequestMethod.GET)
     public ResponseEntity purgeEmailAddress() {
         Map<String, Integer> responseMap = purgeService.purgeEmailAddress();
         return new ResponseEntity(responseMap, HttpStatus.OK);
     }
 
+    /**
+     * Purge exception requests from request_t table. This method will be accesssed from a schduled job.
+     *
+     * @return the response entity
+     */
     @RequestMapping(value = "/purgeExceptionRequests", method = RequestMethod.GET)
     public ResponseEntity purgeExceptionRequests() {
         Map<String, String> responseMap = purgeService.purgeExceptionRequests();
         return new ResponseEntity(responseMap, HttpStatus.OK);
     }
 
+    /**
+     * Purge accession requests URI is used for removing records after batch accession process is completed.
+     *
+     * @return the response entity
+     */
     @RequestMapping(value = "/purgeAccessionRequests", method = RequestMethod.GET)
     public ResponseEntity purgeAccessionRequests() {
         Map<String, String> responseMap = purgeService.purgeAccessionRequests();
