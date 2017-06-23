6264226199e5900f30a06f2c8c3e313065f89285
sudhish74 <sudhish.kutty@htcindia.com>
2017-06-23 17:04:44 +0530

    Code Improvement

--- src/main/java/org/recap/request/RequestParamaterValidatorService.java
+++ src/main/java/org/recap/request/RequestParamaterValidatorService.java
@@ -4,6 +4,8 @@ package org.recap.request;
 import org.recap.ReCAPConstants;
 import org.recap.controller.ItemController;
 import org.recap.model.ItemRequestInformation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
@@ -18,9 +20,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Created by hemalathas on 3/11/16.
