be424dcbbcf8cb0e001cd819f5f44285130c71ed
Hari-Krishnan19 <krish19nov@gmail.com>
2017-08-29 05:26:51 +0530

    Ms-50 Added code to process .gz files and process multiple files at the start of job

--- src/main/java/org/recap/camel/dailyreconciliation/DailyReconciliationRouteBuilder.java
+++ src/main/java/org/recap/camel/dailyreconciliation/DailyReconciliationRouteBuilder.java
@@ -1,8 +1,13 @@
 package org.recap.camel.dailyreconciliation;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Predicate;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.BindyType;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.recap.ReCAPConstants;
 import org.recap.camel.route.StopRouteProcessor;
 import org.recap.model.csv.DailyReconcilationRecord;
@@ -32,6 +37,20 @@ public class DailyReconciliationRouteBuilder {
      * @param ftpPrivateKey                  the ftp private key
      * @param filePath                       the file path
      */
+
+    /**
+     * Predicate to identify is the input file is gz
+     */
+    Predicate gzipFile = new Predicate() {
+        @Override
+        public boolean matches(Exchange exchange) {
+
+            String fileName = (String) exchange.getIn().getHeader(Exchange.FILE_NAME);
+            return StringUtils.equalsIgnoreCase("gz", FilenameUtils.getExtension(fileName));
+
+        }
+    };
+
     public DailyReconciliationRouteBuilder(CamelContext camelContext, ApplicationContext applicationContext,
                                            @Value("${ftp.userName}") String ftpUserName, @Value("${ftp.daily.reconciliation}") String dailyReconciliationFtp,
                                            @Value("${ftp.daily.reconciliation.processed}") String dailyReconciliationFtpProcessed, @Value("${ftp.knownHost}") String ftpKnownHost,
@@ -46,12 +65,36 @@ public class DailyReconciliationRouteBuilder {
                             .routeId(ReCAPConstants.DAILY_RR_FTP_ROUTE_ID)
                             .noAutoStartup()
                             .log("daily reconciliation started")
+                            .choice()
+                            .when(gzipFile)
+                            .unmarshal()
+                            .gzip()
+                            .log("Unzip processed completed for daily reconciliation file")
+                            .process(new Processor() {
+                                @Override
+                                public void process(Exchange exchange) throws Exception {
+                                    String fileName = (String)exchange.getIn().getHeader(Exchange.FILE_NAME);
+                                    exchange.getIn().setHeader(Exchange.FILE_NAME, fileName.replaceFirst(".gz", ".csv"));
+                                }
+                            })
+                            .to(ReCAPConstants.DIRECT+ReCAPConstants.PROCESS_DAILY_RECONCILIATION)
+                            .otherwise()
+                            .to(ReCAPConstants.DIRECT+ReCAPConstants.PROCESS_DAILY_RECONCILIATION)
+                            .end()
+                            .onCompletion()
+                            .choice()
+                            .when(exchangeProperty(ReCAPConstants.CAMEL_BATCH_COMPLETE))
+                            .log("Stopping DailyReconciliation Process")
+                            .process(new StopRouteProcessor(ReCAPConstants.DAILY_RR_FTP_ROUTE_ID));
+
+                    from(ReCAPConstants.DIRECT+ReCAPConstants.PROCESS_DAILY_RECONCILIATION)
                             .unmarshal().bindy(BindyType.Csv, DailyReconcilationRecord.class)
                             .bean(applicationContext.getBean(DailyReconciliationProcessor.class),ReCAPConstants.PROCESS_INPUT)
-                            .onCompletion()
-                            .process(new StopRouteProcessor(ReCAPConstants.DAILY_RR_FTP_ROUTE_ID));
+                            .end();
                 }
             });
+
+
 
             camelContext.addRoutes(new RouteBuilder() {
                 @Override
@@ -63,8 +106,7 @@ public class DailyReconciliationRouteBuilder {
                             .onCompletion()
                             .log("email service started for daily reconciliation")
                             .bean(applicationContext.getBean(DailyReconciliationEmailService.class))
-                            .process(new StopRouteProcessor(ReCAPConstants.DAILY_RR_FS_ROUTE_ID))
-                            .log("daily reconciliation completed");
+                            .process(new StopRouteProcessor(ReCAPConstants.DAILY_RR_FS_ROUTE_ID));
                 }
             });
 
