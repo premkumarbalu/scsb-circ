15f18a1edf60a203b257ed8b6005fd2e7d327200
Prem Kumar <premkumar.balu@htcindia.com>
2017-10-13 13:13:55 +0530

    MS-132 - Added all existing owning ids (item,holding,bib) information for the any owning id mismatches in the report

--- src/main/java/org/recap/service/submitcollection/SubmitCollectionReportHelperService.java
+++ src/main/java/org/recap/service/submitcollection/SubmitCollectionReportHelperService.java
@@ -255,7 +255,7 @@ public class SubmitCollectionReportHelperService {
                         submitCollectionReportInfo.setCustomerCode(incomingItemEntity.getCustomerCode());
                         submitCollectionReportInfo.setOwningInstitution(owningInstitution);
                         String existingOwningInstitutionHoldingsId = getExistingItemEntityOwningInstItemId(fetchedBibliographicEntity,incomingItemEntity);
-                        submitCollectionReportInfo.setMessage(ReCAPConstants.SUBMIT_COLLECTION_FAILED_RECORD+" - Owning institution holding id mismatch - incoming owning institution holdings " +incomingHoldingItemMapEntry.getKey()+ ", existing owning institution item id "+incomingItemEntity.getOwningInstitutionItemId()
+                        submitCollectionReportInfo.setMessage(ReCAPConstants.SUBMIT_COLLECTION_FAILED_RECORD+" - Owning institution holding id mismatch - incoming owning institution holdings id" +incomingHoldingItemMapEntry.getKey()+ ", existing owning institution item id "+incomingItemEntity.getOwningInstitutionItemId()
                                 +", existing owning institution holdings id "+existingOwningInstitutionHoldingsId+", existing owning institution bib id "+fetchedBibliographicEntity.getOwningInstitutionBibId());
                         failureSubmitCollectionReportInfoList.add(submitCollectionReportInfo);
                     }
